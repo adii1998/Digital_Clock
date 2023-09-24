4fe108b129b28920598ffe21a1a03de0ec3cc2cb
adii1998 <102463711+adii1998@users.noreply.github.com>
2023-09-24 19:39:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DigitalClock from './DigitalClock';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Digital Clock</h1>
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
+
